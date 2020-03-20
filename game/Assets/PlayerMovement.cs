d1fe696dc4f7e750f7b73bf613ee41f11a6cedda
Rafał Mandrysz <rafal.mandrysz@gmail.com>
2020-03-20 15:20:09 +0100

    Responding to controlls
    Player character sprite responds to player input (either aroow keys, WASD or gamepad of some sort) and moves accordingly

--- game/Assets/PlayerMovement.cs
+++ game/Assets/PlayerMovement.cs
@@ -4,11 +4,21 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    
+    public float moveSpeed = 5f;
+    public Rigidbody2D rigidbody;
+    private Vector2 movement;
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Input
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        //Movement
+        rigidbody.MovePosition(rigidbody.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
 }
