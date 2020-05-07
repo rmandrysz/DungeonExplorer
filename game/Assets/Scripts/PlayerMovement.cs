d7eadf5321a78c35e35220a99d823d7ee38a41d2
Rafał Mandrysz <rafal.mandrysz@gmail.com>
2020-05-07 13:07:44 +0200

    Animations
    Created and added original animations.
    Also created Player Animation Controller in Unity to handle all the different animations and switch between them. Will need to improve the sprites later on, mainly by adding shadows underneath the Player Character.

---
+++ game/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Space]
+    [Header("Values:")]
+    public float moveSpeed = 4f;
+    
+    private Vector2 movementDirection;
+
+    [Space]
+    [Header("References:")]
+    public Rigidbody2D rb;
+    public Animator animator;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Input
+        GetInput();
+    }
+
+    private void FixedUpdate()
+    {
+        //Movement
+        rb.MovePosition(rb.position + movementDirection * moveSpeed * Time.fixedDeltaTime);
+
+        //Animation
+        animator.SetFloat("Horizontal", movementDirection.x);
+        animator.SetFloat("Vertical", movementDirection.y);
+        animator.SetFloat("Speed", movementDirection.sqrMagnitude);
+
+    }
+
+    private void GetInput()
+    {
+        movementDirection.x = Input.GetAxisRaw("Horizontal");
+        movementDirection.y = Input.GetAxisRaw("Vertical");
+        movementDirection.Normalize();
+    }
+
+}
