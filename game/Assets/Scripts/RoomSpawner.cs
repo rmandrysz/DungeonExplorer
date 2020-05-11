f2e5209da8f736d5b3abae47e12f1ef61dcdd351
Rafał Mandrysz <rafal.mandrysz@gmail.com>
2020-05-11 13:52:02 +0200

    Room templates + script draw
    Added a bunch of room templates to choose from when generating, as well as a draw of a floor generation script. The script or the rooms however are flawed and need fixing to work properly. Currently rooms are spawning at slightly wrong places.

---
+++ game/Assets/Scripts/RoomSpawner.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomSpawner : MonoBehaviour
+{
+    public int doorDir;
+    /*
+    1 --> Left
+    2 --> Top
+    3 --> Right
+    4 --> Bottom
+    */
+
+    private RoomTemplates templates;
+    private int rand;
+    private bool spawned = false;
+
+    private void Start()
+    {
+        templates = GameObject.FindGameObjectWithTag("RoomTemplates").GetComponent<RoomTemplates>();
+        Invoke("Spawn", 0.1f);
+    }
+
+
+    private void Spawn()
+    {
+        if (!spawned)
+        {
+
+            if (doorDir == 1)
+            {
+                rand = Random.Range(0, templates.rightRooms.Length);
+                Instantiate(templates.rightRooms[rand], transform.position, templates.rightRooms[rand].transform.rotation);
+            }
+
+            if (doorDir == 2)
+            {
+                rand = Random.Range(0, templates.bottomRooms.Length);
+                Instantiate(templates.bottomRooms[rand], transform.position, templates.bottomRooms[rand].transform.rotation);
+            }
+
+            if (doorDir == 3)
+            {
+                rand = Random.Range(0, templates.leftRooms.Length);
+                Instantiate(templates.leftRooms[rand], transform.position, templates.leftRooms[rand].transform.rotation);
+            }
+
+            if (doorDir == 4)
+            {
+                rand = Random.Range(0, templates.topRooms.Length);
+                Instantiate(templates.topRooms[rand], transform.position, templates.topRooms[rand].transform.rotation);
+            }
+
+        }
+        spawned = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("SpawnPoint"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ game/Assets/Scripts/RoomTemplates.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomTemplates : MonoBehaviour
+{
+    public GameObject[] leftRooms;
+    public GameObject[] topRooms;
+    public GameObject[] rightRooms;
+    public GameObject[] bottomRooms;
+}
