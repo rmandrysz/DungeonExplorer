1fd214bad9f1281662572dc21c158c008e58c11c
Rafał Mandrysz <rafal.mandrysz@gmail.com>
2020-06-14 20:28:57 +0200

    Added a functioning and updating healthbar
    The player now has a healthbar that updates every time he gets damaged.
    
    I also fixed a very major and frustrating bug caused by using animation events. The player did not restore his movement speed to its base value when the attack animation was interrupted by the animation played when he gets damage himself. Now everything works as intended.

---
+++ game/Assets/Scripts/Healthbar.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Healthbar : MonoBehaviour
+{
+    public Slider slider;
+
+    public void SetHealth(int health)
+    {
+        slider.value = health;
+    }
+}
